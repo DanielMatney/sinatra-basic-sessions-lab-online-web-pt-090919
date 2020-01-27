77f7528f49213af862a1622844ce67edb03b1561
Daniel Matney <danieljmatney@gmail.com>
2020-01-27 18:00:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   configure do
-    set :session_secret
+    enable :sessions
+    set :session_secret, "secret"
+  end
   
   
   
